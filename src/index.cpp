f362cd0593dc36537eae8cc9862add1064cd219c
mr-eyes <mabuelanin@gmail.com>
2021-11-21 18:50:28 -0800

    temporarily disable mqf

--- src/index.cpp
+++ src/index.cpp
@@ -114,7 +114,8 @@ namespace kSpider {
             }else{
                 continue;
             }
-            if(extension == "mqf") {frame = new kDataFrameMQF(detected_kSize, 30, mumur_hasher); break;}
+            // if(extension == "mqf") {frame = new kDataFrameMQF(detected_kSize, 30, mumur_hasher); break;} // temp. switch off
+            if(extension == "extension") {frame = new kDataFramePHMAP(detected_kSize); break;}
             else if(extension == "phmap") {frame = new kDataFramePHMAP(detected_kSize); break;}
             else {continue;}
         }
