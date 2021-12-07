98dfcffafc8c6b5b0370fc46f6768b67298916c0
mr-eyes <mabuelanin@gmail.com>
2021-12-06 19:22:33 -0800

    normalization done

--- apps/normalize_pe.cpp
+++ apps/normalize_pe.cpp
@@ -6,11 +6,11 @@
 int main(int argc, char** argv) {
 
 
-    string PE_1_reads_file = ""; // r1_file_name;
-    string PE_2_reads_file = ""; // r2_file_name;
+    string PE_1_reads_file = argv[1];
+    string PE_2_reads_file = argv[2];
     int chunk_size = 1000;
     int kSize = 25;
-    uint64_t DESIRED_NUM_KMERS = 100;
+    uint64_t DESIRED_NUM_KMERS = 100000000; // 10 MILLIONS
 
     uint64_t stats_total_kmers = 0;
     uint64_t stats_total_kmers_unique = 0;
@@ -23,6 +23,9 @@ int main(int argc, char** argv) {
     std::string base_filename = PE_1_reads_file.substr(PE_1_reads_file.find_last_of("/\\") + 1);
     base_filename = base_filename.substr(0, base_filename.find('_'));
 
+    cout << "SAMPLE: " << base_filename << endl;
+
+
     kmerDecoder* READ_1_KMERS = kmerDecoder::getInstance(PE_1_reads_file, chunk_size, KMERS, mumur_hasher, { {"kSize", kSize} });
     kmerDecoder* READ_2_KMERS = kmerDecoder::getInstance(PE_2_reads_file, chunk_size, KMERS, mumur_hasher, { {"kSize", kSize} });
 
@@ -33,6 +36,7 @@ int main(int argc, char** argv) {
     uint64_t inserted_kmers = 0;
 
     // First iteration: Insert and count the kmers in the kDataFrame
+    cout << "First iteration: Insert and count the kmers in the kDataFrame" << endl;
     while (!READ_1_KMERS->end() && !READ_2_KMERS->end()) {
         READ_1_KMERS->next_chunk();
         READ_2_KMERS->next_chunk();
@@ -58,8 +62,7 @@ int main(int argc, char** argv) {
 
     stats_total_kmers_unique = kf->size();
 
-
-    // Second iteration: remove singletones
+    cout << "Second iteration: remove singletones" << endl;
     auto* kf_singletones_free = new kDataFramePHMAP(KMERS, mumur_hasher, { {"kSize", kSize} });
 
     auto kf_it = kf->begin();
@@ -77,8 +80,13 @@ int main(int argc, char** argv) {
     // Check if the sample size is less than the desired num of kmers
     if (kf_singletones_free->size() < DESIRED_NUM_KMERS) {
         cout << "saving sample (" << base_filename << ")" << endl;
-        kf_singletones_free->save(base_filename);
+        kf_singletones_free->save("normalized_" + base_filename);
         cout << "sample() has (" << kf_singletones_free->size() << ") kmers which is less than the desired (" << DESIRED_NUM_KMERS << ") kmers.";
+        cout << "stats_total_kmers= " << stats_total_kmers << endl;
+        cout << "stats_total_kmers_unique= " << stats_total_kmers_unique << endl;
+        cout << "stats_singletones= " << stats_singletones << endl;
+        cout << "stats_after_singletone_unique= " << stats_after_singletone_unique << endl;
+        cout << "__________________________________"<< endl;
         exit(0);
     }
 
@@ -88,9 +96,9 @@ int main(int argc, char** argv) {
 
     cout << "Third iteration: get only the number of kmers we want" << endl;
     auto* final_kf = new kDataFramePHMAP(KMERS, mumur_hasher, { {"kSize", kSize} });
-    auto kf_singletones_free_it = kf->begin();
+    auto kf_singletones_free_it = kf_singletones_free->begin();
 
-    while (kf_singletones_free_it != kf->end() && final_kf->size() < DESIRED_NUM_KMERS) {
+    while (kf_singletones_free_it != kf_singletones_free->end() && final_kf->size() < DESIRED_NUM_KMERS) {
         final_kf->insert(kf_singletones_free_it.getHashedKmer());
         for (int i = 0; i < step; i++) kf_singletones_free_it++;
     }
@@ -102,5 +110,9 @@ int main(int argc, char** argv) {
     cout << "stats_singletones= " << stats_singletones << endl;
     cout << "stats_after_singletone_unique= " << stats_after_singletone_unique << endl;
     cout << "stats_final_inserted_unique= " << stats_final_inserted_unique << endl;
+
+    final_kf->save("normalized_" + base_filename);
+    cout << "__________________________________"<< endl;
+
 
 }
