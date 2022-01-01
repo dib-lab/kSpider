681a27961f5fb4157cf1a4aaeb90229837fb27f9
mr-eyes <mabuelanin@gmail.com>
2022-01-02 00:35:21 +0200

    minor fix

--- apps/dump_kmer_count.cpp
+++ apps/dump_kmer_count.cpp
@@ -4,6 +4,21 @@
 #include <fstream>
 using namespace std;
 
+string getFileName(const string& s) {
+
+    char sep = '/';
+
+#ifdef _WIN32
+    sep = '\\';
+#endif
+
+    size_t i = s.rfind(sep, s.length());
+    if (i != string::npos) {
+        return(s.substr(i + 1, s.length() - i));
+    }
+
+    return("");
+}
 
 int main(int args, char** argv) {
     string file_name = argv[1];
@@ -12,11 +27,11 @@ int main(int args, char** argv) {
     std::string::size_type idx;
     idx = file_name.rfind('.');
     std::string extension = "";
-
+    string basefile = getFileName(kf_prefix);
 
     auto* loaded_kf = kDataFrame::load(kf_prefix);
 
-    ofstream COUNT(kf_prefix + ".count");
+    ofstream COUNT(basefile + ".count");
 
     auto it = loaded_kf->begin();
     while (it != loaded_kf->end()) {
@@ -24,7 +39,7 @@ int main(int args, char** argv) {
         it++;
     }
 
-  // Close the file
-  COUNT.close();
+    // Close the file
+    COUNT.close();
 
 }
