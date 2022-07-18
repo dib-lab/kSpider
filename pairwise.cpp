8797a25d50c8b6398e8e71fbcc8f16072e259081
mr-eyes <mabuelanin@gmail.com>
2022-07-18 19:06:25 +0200

    pairwise argv

--- pairwise.cpp
+++ pairwise.cpp
@@ -1,6 +1,5 @@
 #include "kSpider.hpp"
 
-int main() {
-    // kSpider::pairwise("/home/mabuelanin/dib-dev/nasa/kSpider_optimize/work_dir/subset_sigs_100k");
-    kSpider::pairwise("/home/mabuelanin/dib-dev/nasa/kSpider_optimize/data/sigs_100k");
+int main(int argc, char** argv) {
+    kSpider::pairwise(argv[1]);
 }
