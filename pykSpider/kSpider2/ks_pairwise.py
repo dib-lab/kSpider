e2d8c940c9cb3b776c2ac07a70523aa5c4ff8eb7
mr-eyes <mabuelanin@gmail.com>
2022-07-20 15:31:45 +0200

    --threads user option

--- pykSpider/kSpider2/ks_pairwise.py
+++ pykSpider/kSpider2/ks_pairwise.py
@@ -4,6 +4,8 @@ from __future__ import division
 
 import sys
 
+from traitlets import default
+
 import _kSpider_internal as kSpider_internal
 
 import click
@@ -12,14 +14,16 @@ from kSpider2.click_context import cli
 
 @cli.command(name="pairwise", help_priority=6)
 @click.option('-i', '--index-prefix', required=True, type=click.STRING, help="Index file prefix")
+@click.option('-t', '--threads', "user_threads", default = 1, required=False, type=int, help="number of cores")
+
 @click.pass_context
-def main(ctx, index_prefix):
+def main(ctx, index_prefix, user_threads):
     """
     Generate containment pairwise matrix.
     """
 
-    ctx.obj.INFO("Constructing the containment pairwise matrix.")
+    ctx.obj.INFO(f"Constructing the containment pairwise matrix using {user_threads} cores.")
 
-    kSpider_internal.pairwise(index_prefix)
+    kSpider_internal.pairwise(index_prefix, user_threads)
 
     ctx.obj.SUCCESS("Done.")
--- src/swig_interfaces/kSpider_internal.i
+++ src/swig_interfaces/kSpider_internal.i
@@ -8,7 +8,7 @@ using namespace std;
 %include std_string.i
 
 namespace kSpider{
-    void pairwise(string index_prefix);
+    void pairwise(string index_prefix, int user_threads);
     void index_kmers(int kSize, string fasta_file, string names_file, int chunk_size, string index_prefix);
     void index_kmers_nonCanonical(int kSize, string fasta_file, string names_file, int chunk_size, string index_prefix);
     void index_skipmers(int m, int n, int k, string fasta_file, string names_file, int chunk_size, string index_prefix);
