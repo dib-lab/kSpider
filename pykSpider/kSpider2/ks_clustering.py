f38981e38d7de5f38699a8b9ffb16099dd8bd9af
mr-eyes <mabuelanin@gmail.com>
2022-12-29 19:29:05 -0800

    support ANI and add some options

--- pykSpider/kSpider2/ks_clustering.py
+++ pykSpider/kSpider2/ks_clustering.py
@@ -10,24 +10,26 @@ from tqdm import tqdm
 class Clusters:
 
     distance_to_col = {
-        "min_cont": 2,
-        "avg_cont": 3,
-        "max_cont": 4,
-        "ani": 5,
+        "min_cont": 3,
+        "avg_cont": 4,
+        "max_cont": 5,
+        "ani": 6,
     }
-    
+
     seq_to_kmers = dict()
     names_map = dict()
 
     def __init__(self, logger_obj, index_prefix, cut_off_threshold, dist_type):
         self.index_prefix = index_prefix
         self.Logger = logger_obj
+        self.dist_type = dist_type
         self.edges_batch_number = 10_000_000
         self.names_file = index_prefix + ".namesMap"
         self.cut_off_threshold = cut_off_threshold
         self.seqToKmers_file = index_prefix + "_kSpider_seqToKmersNo.tsv"
         self.pairwise_file = index_prefix + "_kSpider_pairwise.tsv"
-        self.output = index_prefix + f"_kSpider_clusters_{cut_off_threshold}%.tsv"
+        self.output = index_prefix + \
+            f"_kSpider_clusters_{cut_off_threshold}%.tsv"
         self.shared_kmers_threshold = 200
         self.Logger.INFO("Loading TSV pairwise file")
         self.load_seq_to_kmers(self.seqToKmers_file)
@@ -36,11 +38,9 @@ class Clusters:
             logger_obj.ERROR("unknown distance!")
         self.dist_col = self.distance_to_col[dist_type]
 
-        if self.dist_col == 5:
-            with open(self.pairwise_file, 'r') as pairwise_tsv:
-                if "ani" not in next(pairwise_tsv).lower():
-                    logger_obj.ERROR(
-                        "ANI was selected but was not found in the pairwise file.\nPlease, run kSpider pairwise --extend_with_ani -i <index_prefix> script")
+        if dist_type == "ani":
+            if not os.path.exists(self.index_prefix + "_kSpider_pairwise.ani_col.tsv"):
+                logger_obj.ERROR(f"ANI was selected, but the ani file {self.index_prefix}_kSpider_pairwise.ani_col.tsv was not found!")
 
         self.graph = rx.PyGraph()
         self.nodes_indeces = self.graph.add_nodes_from(
@@ -67,42 +67,70 @@ class Clusters:
         print("[i] constructing graph")
         with open(self.pairwise_file, 'r') as pairwise_tsv:
             next(pairwise_tsv)  # skip header
-            for row in pairwise_tsv:
-                row = row.strip().split('\t')
-                seq1 = int(row[0]) - 1
-                seq2 = int(row[1]) - 1
-                distance = float(row[self.dist_col]) * 100
+            if self.dist_type == "ani":
+                with open(self.index_prefix + "_kSpider_pairwise.ani_col.tsv") as ani_col_file:
+                    for row in pairwise_tsv:
+                        row = row.strip().split('\t')
+                        seq1 = int(row[0]) - 1
+                        seq2 = int(row[1]) - 1
+                        distance = float(next(ani_col_file).strip()) * 100
 
-                # don't make graph edge
-                if distance < self.cut_off_threshold:
-                    continue
+                        # don't make graph edge
+                        if distance < self.cut_off_threshold:
+                            continue
 
-                if batch_counter < self.edges_batch_number:
-                    batch_counter += 1
-                    edges_tuples.append((seq1, seq2, distance))
-                else:
-                    self.graph.add_edges_from(edges_tuples)
-                    batch_counter = 0
-                    edges_tuples.clear()
+                        if batch_counter < self.edges_batch_number:
+                            batch_counter += 1
+                            edges_tuples.append((seq1, seq2, distance))
+                        else:
+                            self.graph.add_edges_from(edges_tuples)
+                            batch_counter = 0
+                            edges_tuples.clear()
 
+                    else:
+                        if len(edges_tuples):
+                            self.graph.add_edges_from(edges_tuples)
             else:
-                if len(edges_tuples):
-                    self.graph.add_edges_from(edges_tuples)
+                for row in pairwise_tsv:
+                    row = row.strip().split('\t')
+                    seq1 = int(row[0]) - 1
+                    seq2 = int(row[1]) - 1
+                    distance = float(row[self.dist_col]) * 100
+
+                    # don't make graph edge
+                    if distance < self.cut_off_threshold:
+                        continue
+
+                    if batch_counter < self.edges_batch_number:
+                        batch_counter += 1
+                        edges_tuples.append((seq1, seq2, distance))
+                    else:
+                        self.graph.add_edges_from(edges_tuples)
+                        batch_counter = 0
+                        edges_tuples.clear()
+
+                else:
+                    if len(edges_tuples):
+                        self.graph.add_edges_from(edges_tuples)
 
     def cluster_graph(self):
         self.connected_components = rx.connected_components(self.graph)
-        self.Logger.INFO(f"number of clusters: {len(self.connected_components)}")
+        self.Logger.INFO(
+            f"number of clusters: {len(self.connected_components)}")
         single_components = 0
-        retworkx_export = self.index_prefix + f"_kSpider_graph_{self.cut_off_threshold}%.json"
-        self.Logger.INFO(f"writing {retworkx_export}") # and {self.output} ...")
-        # rx.node_link_json(self.graph, path = retworkx_export)        
+        retworkx_export = self.index_prefix + \
+            f"_kSpider_graph_{self.cut_off_threshold}%.json"
+        # and {self.output} ...")
+        self.Logger.INFO(f"writing {retworkx_export}")
+        # rx.node_link_json(self.graph, path = retworkx_export)
         with open(self.output, 'w') as CLUSTERS:
             for component in self.connected_components:
-            # uncomment to exclude single genome clusters from exporting
-            # if len(component) == 1:
-            #     single_components += 1
-            #     continue
-                named_component = [self.names_map[node + 1] for node in component]
+                # uncomment to exclude single genome clusters from exporting
+                # if len(component) == 1:
+                #     single_components += 1
+                #     continue
+                named_component = [self.names_map[node + 1]
+                                   for node in component]
                 CLUSTERS.write(','.join(named_component) + '\n')
 
 
@@ -119,12 +147,13 @@ New help messages
 @cli.command(name="cluster", help_priority=4)
 @click.option('-c', '--cutoff', required=False, type=click.FloatRange(0, 1, clamp=False), default=0.0, show_default=True, help="cluster sequences with (containment > cutoff)")
 @click.option('-i', '--index-prefix', "index_prefix", required=True, type=click.STRING, help="Index file prefix")
-@click.option('-d', '--dist-type', "distance_type", required=False, default="max_cont", type=click.STRING, help="select from ['min_containment', 'avg_containment', 'max_containment', 'ani']")
+@click.option('-d', '--dist-type', "distance_type", required=False, default="max_cont", show_default=True, type=click.STRING, help="select from ['min_containment', 'avg_containment', 'max_containment', 'ani']")
 @click.pass_context
 def main(ctx, index_prefix, cutoff, distance_type):
     """Sequence clustering."""
     cutoff = float(cutoff) * 100
-    kCl = Clusters(logger_obj=ctx.obj, index_prefix=index_prefix, cut_off_threshold=cutoff, dist_type=distance_type)
+    kCl = Clusters(logger_obj=ctx.obj, index_prefix=index_prefix,
+                   cut_off_threshold=cutoff, dist_type=distance_type)
     ctx.obj.INFO("Building the main graph...")
     kCl.construct_graph()
     ctx.obj.INFO("Clustering...")
