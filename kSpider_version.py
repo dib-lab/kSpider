85a6fe3cd9e1ef2d95de118d4c1ef464975bb6ad
mr-eyes <mabuelanin@gmail.com>
2023-02-20 18:55:08 -0800

    version

--- kSpider_version.py
+++ kSpider_version.py
@@ -3,10 +3,12 @@ import json
 import urllib.request
 import sys
 
+#TODO there are two files with kSpider_version, remove one.
+
 # Only update this when releasing stable
 MAJOR = 2
 MINOR = 3
-PATCH = 1
+PATCH = 2
 
 PYPI_PACKAGE = "kSpider"
 
